40173eab8843895047dcba3e889025674e5928cc
bcorbold <bradencorbold@gmail.com>
2018-02-27 14:04:03 -0500

    style: keep adding to the card template style: add in date header bar for separation of dates

--- index.js
+++ index.js
@@ -21,18 +21,39 @@ function fetchTransactions() {
 
 function renderTransactionList(transactionList) {
   const transactionListContainer = $('.transactions-container');
-  transactionList.forEach(t => {
-    transactionListContainer.append(createTransactionRowTemplate(t));
+
+  transactionList.forEach((t, i) => {
+    if (i === 0 || t.transactionDate !== transactionList[i-1].transactionDate) {
+      transactionListContainer.append(createDateHeader(t.transactionDate))
+    }
+    const card = createTransactionRowTemplate(t);
+    transactionListContainer.append(card);
   });
+}
+
+function createDateHeader(date) {
+  return `<div style="width: 100%; padding: 4px 16px; background-color: #F5F5F5; margin-top: 4px;">
+            <p style="margin: 0;">${date}</p>
+          </div>`;
 }
 
 function createTransactionRowTemplate(transaction) {
   const accountName = fetchedData.accounts.filter(a => a.accountId === transaction.accountId)[0].accountName;
-  const card = `<div style="width: 100%; margin-bottom: 2px; padding: 8px; background-color: white;">
-                    <p style="margin: 0;">${accountName}</p>
+  const card = `<div style="width: 100%; margin-bottom: 2px; padding: 8px 16px; background-color: white; display: flex; flex-direction: row">
+                    <div style="flex-grow: 1; margin-right: 16px;">
+                      <p style="margin: 0; font-weight: 600;">${accountName}</p>
+                      <i style="margin: 0;">${transaction.description}</i>
+                    </div>
+                    <div style="text-align: right;">
+                      <p style="margin: 0; font-size: 20px;">$${transaction.amount.toFixed(2)}</p>
+                      <p style="margin: 0; font-size: 12px; font-weight: 300;">$${transaction.runningBalance.toFixed(2)}</p>
+                    </div>
                 </div>`;
 
   return card;
 }
+
+// todo: could initially create templates for all transaction cards and then add/remove templates as needed
+// todo: can this be done lazily? Only rendering down the list as they come into view?
 
 init();
