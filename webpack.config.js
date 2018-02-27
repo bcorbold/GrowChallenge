167a675ec18ff73ca367fb7817b229de7bc598f4
bcorbold <bradencorbold@gmail.com>
2018-02-27 16:53:17 -0500

    build: simplified webpack process

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,24 @@
-const path = require('path');
 
 module.exports = {
-  entry: "./index.js",
+  entry: `${__dirname}/src/app.js`,
   output: {
-    path: path.resolve(__dirname, './'),
-    filename: "bundle.js"
+    path: `${__dirname}`,
+    filename: 'bundle.js'
   },
   module:{
     loaders: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "eslint-loader",
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader'
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         query: {
-          presets: ["es2015"]
+          presets: ['es2015']
         }
       }
     ]
-  },
-  devtool: "inline-source-map"
+  }
 };
