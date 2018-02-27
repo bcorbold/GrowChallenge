6a1deb0686373a40205023c9574dad6df233d046
bcorbold <bradencorbold@gmail.com>
2018-02-26 22:54:29 -0500

    core: set up of basic project style: playing around the MDL for theme/components

---
+++ src/main.js
@@ -0,0 +1,10 @@
+let transactions;
+const transactionUrl = 'http://demo7235469.mockable.io/transactions';
+
+function fetchTransactions() {
+  // fetch(transactionUrl).then(response => response.json())
+  //     .then(data => transactions = data)
+  //     .catch(error => console.error(error));fetch(transactionUrl).then(response => response.json())
+  //     .then(data => transactions = data)
+  //     .catch(error => console.error(error));
+}
