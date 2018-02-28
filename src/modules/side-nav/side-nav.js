eb6d57cba7648568ff7ad0d72dc5258c6eeb5b9a
bcorbold <bradencorbold@gmail.com>
2018-02-27 23:13:31 -0500

    core: keep track of selected accounts that will be used to filter transaction

--- src/modules/side-nav/side-nav.js
+++ src/modules/side-nav/side-nav.js
@@ -1,28 +1,29 @@
 require('./side-nav.scss');
 
 import $ from 'jquery';
+import _ from 'lodash';
 
-const sideNaveTemplate = `
-<div id="mySidenav" class="sidenav">
-  <i class="closebtn material-icons" id="closeNav">clear</i>
-  <div class="accounts-select">
-    <i class="material-icons account-filter-button">add</i>
-    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-      <input class="mdl-textfield__input" type="text" id="sample3">
-      <label class="mdl-textfield__label" for="sample3">Accounts</label>
-    </div>
-  </div>
-  
-  <div id="selected-accounts" class="selected-accounts-list"></div>
-</div>
-`;
+const filteredAccounts = [];
+
+const sideNaveTemplate = `<div id="mySidenav" class="sidenav">
+                            <i class="closebtn material-icons" id="closeNav">clear</i>
+                            <div class="accounts-select">
+                              <i class="material-icons account-filter-button">add</i>
+                              <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                                <input class="mdl-textfield__input" type="text" id="accountFilter">
+                                <label class="mdl-textfield__label" for="sample3">Accounts</label>
+                              </div>
+                            </div>
+                            
+                            <div id="selected-accounts" class="selected-accounts-list"></div>
+                          </div>`;
 
 export function createSideNav() {
   $('body').append(sideNaveTemplate);
 
   document.getElementById('openNav').addEventListener('click', () => openNav());
   document.getElementById('closeNav').addEventListener('click', () => closeNav());
-  document.getElementsByClassName('account-filter-button')[0].addEventListener('click', () => renderChip());
+  document.getElementsByClassName('account-filter-button')[0].addEventListener('click', () => addAccountFilterChip());
 }
 
 export function openNav() {
@@ -33,8 +34,13 @@ export function closeNav() {
   document.getElementById('mySidenav').style.width = '0';
 }
 
-function renderChip() {
-  $('#selected-accounts').append(createChip('test'));
+function addAccountFilterChip() {
+  const accountName = $('#accountFilter').val();
+
+  if (!filteredAccounts.includes(accountName)) {
+    filteredAccounts.push(accountName);
+    $('#selected-accounts').append(createChip(accountName));
+  }
 }
 
 function createChip(text) {
@@ -43,6 +49,7 @@ function createChip(text) {
                             <button type="button" class="mdl-chip__action"><i class="material-icons">cancel</i></button>
                         </span>`);
   chipTemplate.on('click', function(event) {
+    _.pull(filteredAccounts, event.currentTarget.children[0].childNodes[0].data); // if the template changes so will this
     event.currentTarget.remove();
   });
   return chipTemplate;
