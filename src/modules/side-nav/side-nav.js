a1d59d9b369e4d1ce49d18b957db0638abe15d07
bcorbold <bradencorbold@gmail.com>
2018-02-27 22:48:04 -0500

    style: add momentum to scrolling core: added removable `chip-list` to be hooked up with fitlers

--- src/modules/side-nav/side-nav.js
+++ src/modules/side-nav/side-nav.js
@@ -4,11 +4,16 @@ import $ from 'jquery';
 
 const sideNaveTemplate = `
 <div id="mySidenav" class="sidenav">
-  <a href="javascript:void(0)" id="closeNav" class="closebtn" >&times;</a>
-  <a href="#">About</a>
-  <a href="#">Services</a>
-  <a href="#">Clients</a>
-  <a href="#">Contact</a>
+  <i class="closebtn material-icons" id="closeNav">clear</i>
+  <div class="accounts-select">
+    <i class="material-icons account-filter-button">add</i>
+    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+      <input class="mdl-textfield__input" type="text" id="sample3">
+      <label class="mdl-textfield__label" for="sample3">Accounts</label>
+    </div>
+  </div>
+  
+  <div id="selected-accounts" class="selected-accounts-list"></div>
 </div>
 `;
 
@@ -17,6 +22,7 @@ export function createSideNav() {
 
   document.getElementById('openNav').addEventListener('click', () => openNav());
   document.getElementById('closeNav').addEventListener('click', () => closeNav());
+  document.getElementsByClassName('account-filter-button')[0].addEventListener('click', () => renderChip());
 }
 
 export function openNav() {
@@ -25,4 +31,19 @@ export function openNav() {
 
 export function closeNav() {
   document.getElementById('mySidenav').style.width = '0';
+}
+
+function renderChip() {
+  $('#selected-accounts').append(createChip('test'));
+}
+
+function createChip(text) {
+  const chipTemplate = $(`<span class="mdl-chip mdl-chip--deletable">
+                            <span class="mdl-chip__text account-filter">${text}</span>
+                            <button type="button" class="mdl-chip__action"><i class="material-icons">cancel</i></button>
+                        </span>`);
+  chipTemplate.on('click', function(event) {
+    event.currentTarget.remove();
+  });
+  return chipTemplate;
 }
