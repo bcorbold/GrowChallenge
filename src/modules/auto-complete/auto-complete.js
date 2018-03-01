7a251b9419dd5a23aac44816ae05207b3ceafba5
bcorbold <bradencorbold@gmail.com>
2018-03-01 16:43:56 -0500

    chore: continue cleaning up auto complete code

--- src/modules/auto-complete/auto-complete.js
+++ src/modules/auto-complete/auto-complete.js
@@ -2,6 +2,10 @@ require('./auto-complete.scss');
 
 import $ from 'jquery';
 
+const keyDown = 40;
+const keyUp = 38;
+const keyEnter = 13;
+
 export function createAutoCompleteInput(parentId, inputId, label) {
   const template = `
     <form autocomplete="off" class="auto-complete-form" id="${inputId}Form">
@@ -27,97 +31,80 @@ export function populateAutoComplete(inputId, filterList) {
 // todo: match based on contents of string instead of just matching the start
 
 // auto complete functionality was modified based on https://www.w3schools.com/howto/howto_js_autocomplete.asp
-function autocomplete(inputElement, options) { // inp = input element, arr = possible list
-  let currentFocus;
+function autocomplete(inputElement, options) {
 
   // filter on input
-  inputElement.addEventListener("input", (e) => {
-    let a, b, i, val = inputElement.value;
+  inputElement.addEventListener('input', (event) => {
+    let inputValue = inputElement.value;
     closeAllLists();
 
-    if (!val) { return false;}
-    currentFocus = -1;
+    if (!inputValue) { return false;}
+    let currentFocus = -1;
 
     // todo: can do this in a better way?
     // build div that will contain all the potential values
-    a = document.createElement('DIV');
-    a.setAttribute('id', inputElement.id + 'autocomplete-list');
-    a.setAttribute('class', 'autocomplete-items');
-    inputElement.parentNode.appendChild(a);
+    const optionsContainer = document.createElement('DIV');
+    optionsContainer.setAttribute('id',`${inputElement.id}autocomplete-list`);
+    optionsContainer.setAttribute('class', 'autocomplete-items');
+    inputElement.parentNode.appendChild(optionsContainer);
 
-    for (i = 0; i < options.length; i++) {
-
+    for (let i = 0; i < options.length; i++) {
       // todo: right now just compares the start, switch to regex for contents of string
-      if (options[i].substr(0, val.length).toUpperCase() === val.toUpperCase()) {
-
+      if (options[i].substr(0, inputValue.length).toUpperCase() === inputValue.toUpperCase()) {
         // build up option div
-        b = document.createElement("DIV");
-        b.innerHTML = `<strong>${options[i].substr(0, val.length)}</strong>${options[i].substr(val.length)}
+        const selectOptionContainer = document.createElement('DIV');
+        selectOptionContainer.innerHTML = `<strong>${options[i].substr(0, inputValue.length)}</strong>${options[i].substr(inputValue.length)}
                        <input type="hidden" value="${options[i]}">`;
 
-        b.addEventListener("click", function(e) {
-          /*insert the value for the autocomplete text field:*/
-          inputElement.value = this.getElementsByTagName("input")[0].value;
-          /*close the list of autocompleted values,
-          (or any other open lists of autocompleted values:*/
+        // handle option being clicked
+        selectOptionContainer.addEventListener('click', function(e) {
+          inputElement.value = this.getElementsByTagName('input')[0].value;
           closeAllLists();
         });
-        a.appendChild(b);
+
+        optionsContainer.appendChild(selectOptionContainer);
       }
     }
   });
 
-  /*execute a function presses a key on the keyboard:*/
-  inputElement.addEventListener("keydown", function(e) {
-    let x = document.getElementById(this.id + "autocomplete-list");
-    if (x) x = x.getElementsByTagName("div");
-    if (e.keyCode == 40) {
-      /*If the arrow DOWN key is pressed,
-      increase the currentFocus variable:*/
+  // highlight selections based on key presses
+  inputElement.addEventListener('keydown', function(e) {
+    let x = document.getElementById(this.id + 'autocomplete-list');
+    if (x) x = x.getElementsByTagName('div');
+    if (e.keyCode === keyDown) {
       currentFocus++;
-      /*and and make the current item more visible:*/
       addActive(x);
-    } else if (e.keyCode == 38) { //up
-      /*If the arrow UP key is pressed,
-      decrease the currentFocus variable:*/
+    } else if (e.keyCode === keyUp) {
       currentFocus--;
-      /*and and make the current item more visible:*/
       addActive(x);
-    } else if (e.keyCode == 13) {
-      /*If the ENTER key is pressed, prevent the form from being submitted,*/
-      e.preventDefault();
+    } else if (e.keyCode === keyEnter) { // todo: add the input to the selected list if valid
+      e.preventDefault(); // stops from submitting the form
       if (currentFocus > -1) {
-        /*and simulate a click on the "active" item:*/
-        if (x) x[currentFocus].click();
+        if (x) x[currentFocus].click(); // simulate click on item
       }
     }
   });
 
-  function addActive(x) {
-    /*a function to classify an item as "active":*/
-    if (!x) return false;
-    /*start by removing the "active" class on all items:*/
-    removeActive(x);
-    if (currentFocus >= x.length) currentFocus = 0;
-    if (currentFocus < 0) currentFocus = (x.length - 1);
-    /*add class "autocomplete-active":*/
-    x[currentFocus].classList.add("autocomplete-active");
+  // active being the element that is highlighted
+  function addActive(elementToCheck) {
+    if (!elementToCheck) return false;
+    removeActive(elementToCheck);
+    if (currentFocus >= elementToCheck.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (elementToCheck.length - 1);
+    elementToCheck[currentFocus].classList.add('autocomplete-active');
   }
 
-  function removeActive(x) {
-    /*a function to remove the "active" class from all autocomplete items:*/
-    for (let i = 0; i < x.length; i++) {
-      x[i].classList.remove("autocomplete-active");
+  function removeActive(elements) {
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].classList.remove('autocomplete-active');
     }
   }
 
-  function closeAllLists(elmnt) {
-    /*close all autocomplete lists in the document,
-    except the one passed as an argument:*/
-    let x = document.getElementsByClassName("autocomplete-items");
-    for (let i = 0; i < x.length; i++) {
-      if (elmnt != x[i] && elmnt != inputElement) {
-        x[i].parentNode.removeChild(x[i]);
+  function closeAllLists(selectedElement) {
+    let autoCompleteItems = document.getElementsByClassName('autocomplete-items');
+    for (let i = 0; i < autoCompleteItems.length; i++) {
+      if (selectedElement !== autoCompleteItems[i] && selectedElement !== inputElement) {
+        autoCompleteItems[i].parentNode.removeChild(autoCompleteItems[i]);
       }
     }
   }
